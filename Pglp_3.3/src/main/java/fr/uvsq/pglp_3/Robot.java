4c38015e0af97a3131473a70162f23255192af3e
Hanitra <andritsalamaramaroson@gmail.com>
2020-03-09 11:16:31 +0100

    Robot avance

---
+++ Pglp_3.3/src/main/java/fr/uvsq/pglp_3/Robot.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.uvsq.pglp_3;
+
+import com.sun.javafx.scene.traversal.Direction;
+import javax.swing.text.Position;
+
+/**
+ *
+ * @author andri
+ */
+public class Robot {
+    private Position position;
+    private Direction direction;
+    
+    public void tourne(){    }
+    
+}
---
+++ Pglp_3.3/src/main/java/fr/uvsq/pglp_3/RobotAvance.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.uvsq.pglp_3;
+
+/**
+ *
+ * @author andri
+ */
+public class RobotAvance extends Robot{
+    public void avance(){}
+    
+}
